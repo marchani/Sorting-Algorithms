6cf2cde3f14ae173b72a9610ac174983cab2a2ed
marchani <carlos.marchani@gmail.com>
2019-08-05 02:19:37 -0500

    Initial Commit.

---
+++ SortingAlgorithms/main.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <limits>
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+
+//
+// partition()
+//
+int partition(vector< int >& array1, int lowIndex, int highIndex)
+{
+	int pivot = array1[highIndex];
+	int i = lowIndex;
+
+	for (int j = lowIndex; j < highIndex; j++)
+	{
+		if (array1[j] < pivot)
+		{
+			// Swap A[i] with A[j].
+			int temp = array1[i];
+			array1[i] = array1[j];
+			array1[j] = temp;
+
+			i++;
+		}
+	}
+
+	// Swap A[i] with A[hi].
+	int temp = array1[i];
+	array1[i] = array1[highIndex];
+	array1[highIndex] = temp;
+
+	return i;
+}
+
+
+//
+// quicksort()
+//
+void quicksort(vector< int >& unsortedArray, int lowIndex, int highIndex)
+{
+	if (lowIndex < highIndex)
+	{
+		int p = partition(unsortedArray, lowIndex, highIndex);
+
+		quicksort(unsortedArray, lowIndex, p - 1);
+		quicksort(unsortedArray, p + 1, highIndex);
+	}
+}
+
+
+//
+// combine()
+//
+void combine(vector< int >& array1, int lowIndex, int middleIndex, int highIndex)
+{
+	vector< int > L;
+	vector< int > R;
+
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < (middleIndex - lowIndex + 1); i++)
+	{
+		L.push_back(array1[lowIndex + i]);
+	}
+
+	for (j = 0; j < (highIndex - middleIndex); j++)
+	{
+		R.push_back(array1[middleIndex + j + 1]);
+	}
+
+	L.push_back(std::numeric_limits< int >::max());
+	R.push_back(std::numeric_limits< int >::max());
+
+	i = 0;
+	j = 0;
+
+	for (int k = lowIndex; k <= highIndex; k++)
+	{
+		if (L[i] <= R[j])
+		{
+			array1[k] = L[i];
+			i++;
+		}
+		else
+		{
+			array1[k] = R[j];
+			j++;
+		}
+	}
+}
+
+
+//
+// mergeSort()
+//
+void mergeSort(vector< int >& unsortedArray, int lowIndex, int highIndex)
+{
+	if (lowIndex < highIndex)
+	{
+		int middleIndex = (lowIndex + highIndex) / 2;
+
+		mergeSort(unsortedArray, lowIndex, middleIndex);
+		mergeSort(unsortedArray, middleIndex + 1, highIndex);
+		combine(unsortedArray, lowIndex, middleIndex, highIndex);
+	}
+}
+
+
+//
+// printArray()
+//
+void printArray(const vector< int >& arr)
+{
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cout << arr[i] << ' ';
+	}
+}
+
+
+//
+// main()
+//
+int main()
+{
+	/* EXAMPLE 1: Quicksort */
+
+	cout << "Example 1: Quicksort" << endl;
+
+	vector< int > unsortedArray1 = { 0, 2, 3, 1, 2, 0, 2, 3, 1, 2 };
+
+	quicksort(unsortedArray1, 0, unsortedArray1.size() - 1);
+
+	printArray(unsortedArray1);
+
+	cout << endl << endl;
+
+
+	/* EXAMPLE 2: Merge Sort */
+	
+	cout << "Example 2: Merge Sort" << endl;
+
+	vector< int > unsortedArray2 = { 0, 2, 3, 1, 2, 0, 2, 3, 1, 2 };
+
+	mergeSort( unsortedArray2, 0, unsortedArray2.size() - 1 );
+
+	printArray( unsortedArray2 );
+	
+	return 0;
+}
